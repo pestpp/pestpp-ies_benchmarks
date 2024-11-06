cfa4d4267d9629fe2380ba535554cf8938e5228b
jwhite <jtwhite1000@gmail.com>
2024-11-06 16:45:55 -0700

    fix for new n_iter_mean scheme

--- ies_test_part4.py
+++ ies_test_part4.py
@@ -2335,10 +2335,14 @@ def tenpar_mean_iter_test():
     shutil.copytree(template_d,test_d)
     pst_name = "pest.pst"
     pst = pyemu.Pst(os.path.join(template_d,pst_name))
-    pst.pestpp_options["ies_n_iter_mean"] = 9
+    pst.pestpp_options["ies_n_iter_mean"] = 3
     pst.pestpp_options["ies_initial_lambda"] = -100
-    pst.control_data.noptmax = 12
-    pst.pestpp_options["ies_num_reals"] = 10
+    pst.control_data.noptmax = 21 # hard coded to test results below
+    pst.pestpp_options["ies_num_reals"] = 50
+    pst.pestpp_options["ies_debug_fail_remainder"] = True
+    pst.pestpp_options["ies_debug_fail_subset"] = True
+    
+    
     pst.write(os.path.join(test_d,pst_name))
     pyemu.os_utils.run("{0} {1}".format(exe_path,pst_name),cwd=test_d)
     #pyemu.os_utils.start_workers(template_d, exe_path, pst_name, num_workers=5,
@@ -2350,14 +2354,10 @@ def tenpar_mean_iter_test():
     
     phidf = pd.read_csv(os.path.join(test_d,"pest.phi.actual.csv"),index_col=0)
     print(phidf.loc[:,"mean"])
-    assert phidf.shape[0] == pst.control_data.noptmax + 2 #plus for prior and plus one since the mean shift process increments things
-    assert phidf.loc[pst.pestpp_options["ies_n_iter_mean"]+1,"mean"] > phidf.loc[pst.pestpp_options["ies_n_iter_mean"],"mean"]
-    pst.pestpp_options.pop("ies_n_iter_mean")
-    pst.write(os.path.join(test_d1,pst_name))
-    pyemu.os_utils.run("{0} {1}".format(exe_path,pst_name),cwd=test_d1)
-    phidf = pd.read_csv(os.path.join(test_d1,"pest.phi.actual.csv"),index_col=0)
     print(phidf.shape)
-    assert phidf.shape[0] < pst.control_data.noptmax - 1
+    assert phidf.shape[0] == 29 #hard coded to noptmax above
+    assert phidf.shape[1] == 55 #50 reals + summary stats
+
 
 def twopar_freyberg_resp_surface_invest():
     model_d = "twopar_freyberg"
@@ -2646,12 +2646,13 @@ def tenpar_noise_invest():
     
 
 if __name__ == "__main__":
-    tenpar_adjust_weights_test()
+    #multimodal_test()
+    #tenpar_adjust_weights_test()
     #tenpar_noise_invest()
     #shutil.copy2(os.path.join("..","exe","windows","x64","Debug","pestpp-ies.exe"),os.path.join("..","bin","win","pestpp-ies.exe"))
     #twopar_freyberg_resp_surface_invest()
     #plot_twopar_resp_results()
-    #tenpar_mean_iter_test()
+    tenpar_mean_iter_test()
     #freyberg_center_on_test()
     #freyberg_rcov_test()
     #tenpar_upgrade_on_disk_test_weight_ensemble_test()
